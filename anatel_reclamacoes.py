0cda535a14b4ae89eed79890fc811757305d00a3
Gabriel Prata <119508139+gabrielmprata@users.noreply.github.com>
2024-09-24 09:12:06 -0300

    Add files via upload

--- anatel_reclamacoes.py
+++ anatel_reclamacoes.py
@@ -71,7 +71,7 @@ st.markdown("""
 df_pesquisa = pd.read_csv('datasets/df_pesquisa.csv.bz2')
 
 # Construção dos Datasets
-
+# 1. Histórico indicadores
 df_historico_ind = (df_pesquisa[['ano', "indicador", 'nota']]
                     [(df_pesquisa['grupo'] == 'Indicadores') &
                      (df_pesquisa['calculo'] == 'Média Índices') &
@@ -93,11 +93,72 @@ var_dig = (df_historico_ind.nota.values[14] -
 var_cob = (df_historico_ind.nota.values[12] -
            df_historico_ind.nota.values[6]).round(2)
 
+# 2. Satisfação Geral por Prestadora
+df_isg_prestadora = (df_pesquisa[["prestadora", 'nota']]
+                     [(df_pesquisa['grupo'] == 'Indicadores') &
+                     (df_pesquisa['calculo'] == 'Média Índices') &
+                     (df_pesquisa['indicador'] == 'Satisfação Geral') &
+                     (df_pesquisa['ano'] == 2023) &
+                     (df_pesquisa['prestadora'] != 'Média Prestadoras') &
+                     (df_pesquisa['estado'] != 'Média Brasil')
+                      ]
+                     ).groupby(["prestadora"])['nota'].mean().round(2).reset_index()
+
+# 2.1 Média Brasil por Prestadora
+df_isg_media = (df_pesquisa[["prestadora", 'nota']]
+                [(df_pesquisa['grupo'] == 'Indicadores') &
+                 (df_pesquisa['calculo'] == 'Média Índices') &
+                 (df_pesquisa['indicador'] == 'Satisfação Geral') &
+                 (df_pesquisa['ano'] == 2023) &
+                 (df_pesquisa['prestadora'] == 'Média Prestadoras') &
+                 (df_pesquisa['estado'] == 'Média Brasil')
+                 ]
+                ).groupby(["prestadora"])['nota'].mean().round(2).reset_index()
+
+# 2.2 Criando texto para a anotação no gráfico
+df_isg_media['annot'] = "Média Brasil" + ' ' + df_isg_media['nota'].astype(str)
+
+# 2.3 Por estado
+
+df_isg_media_uf = (df_pesquisa[["estado", 'nota']]
+                   [(df_pesquisa['grupo'] == 'Indicadores') &
+                    (df_pesquisa['calculo'] == 'Média Índices') &
+                    (df_pesquisa['indicador'] == 'Satisfação Geral') &
+                    (df_pesquisa['ano'] == 2023) &
+                    (df_pesquisa['prestadora'] != 'Média Prestadoras') &
+                    (df_pesquisa['estado'] != 'Média Brasil')
+                    ]
+                   ).groupby(["estado"])['nota'].mean().round(2).reset_index()
+
+# 3. Notas das Perguntas que compõe o Índice de Satisfação Geral e Qualidade
+# 3.1 ISG
+df_isg_perguntas = (df_pesquisa[["tema", 'nota']]
+                    [(df_pesquisa['grupo'] == 'Indicadores') &
+                     (df_pesquisa['calculo'] == 'Média Ponderada') &
+                     (df_pesquisa['indicador'] == 'Satisfação Geral') &
+                     (df_pesquisa['ano'] == 2023) &
+
+                     (df_pesquisa['prestadora'] != 'Média Prestadoras') &
+                     (df_pesquisa['estado'] != 'Média Brasil')
+                     ]
+                    ).groupby(["tema"])['nota'].mean().round(2).reset_index()
+
+# 3.2 Perguntas qualidade
+df_isg_qualidade = (df_pesquisa[["indicador", "tema", 'nota']]
+                    [(df_pesquisa['grupo'] == 'Indicadores') &
+                     (df_pesquisa['calculo'] == 'Média Ponderada') &
+                     (df_pesquisa['indicador'] != 'Satisfação Geral') &
+                     (df_pesquisa['ano'] == 2023) &
+
+                     (df_pesquisa['prestadora'] != 'Média Prestadoras') &
+                     (df_pesquisa['estado'] != 'Média Brasil')
+                     ]
+                    ).groupby(["indicador", "tema"])['nota'].mean().round(2).reset_index()
 
 #######################
 # Construção dos Gráficos
 
-# Histórico indicadores
+# 1. Histórico indicadores
 hist = px.histogram(df_historico_ind, x="indicador", y="nota", color='ano', barmode='group',
                     labels=dict(indicador="Indicador",
                                 ano="Ano", nota="Nota"),
@@ -108,7 +169,91 @@ hist.update_traces(textfont_size=12, textangle=0,
                    textposition="outside", cliponaxis=False)
 hist.update_layout(yaxis_title="Nota")
 
+# 2. Satisfação por operadora
+oper = px.bar(df_isg_prestadora.sort_values(by='nota', ascending=False), x='nota', y='prestadora', color='prestadora', orientation='h',
+              labels=dict(prestadora="Prestadora", nota="Nota"),
 
+              color_discrete_sequence=["#fff666"],
+              template="plotly_dark",  text_auto='.2s'
+
+              )
+oper.update_layout(showlegend=False)
+oper.add_vline(x=df_isg_media.nota.values[0], line_width=3, annotation_text=df_isg_media.annot.values[0],
+               annotation_position="top",
+               line_dash="dot", line_color="green")
+
+# 2.1 Por estado
+estado = px.bar(df_isg_media_uf.sort_values(by='estado', ascending=True), x="estado", y="nota",
+                labels=dict(estado="Estado", regiao="Região", nota="Nota"),
+                color_discrete_sequence=["#fff666"], template="plotly_dark"
+                )
+estado.add_hline(y=df_isg_media.nota.values[0], line_width=3, annotation_text=df_isg_media.annot.values[0],
+                 annotation_position="top right", annotation_font_size=16,
+                 line_dash="dot", line_color="green")
+
+# 3 Perguntas ISG
+isg = px.bar(df_isg_perguntas.sort_values(by='tema', ascending=False), x='nota', y='tema', color='tema', orientation='h',
+             labels=dict(tema="Tema", nota="Nota"),
+             color_discrete_sequence=["#fff666"],
+             template="plotly_dark",  text_auto='.3s'
+             )
+isg.update_layout(showlegend=False)
+
+# 3.1 Perguntas qualidade (1 gráfico por pergunta, total de 5)
+qual1 = px.bar(df_isg_qualidade.query('indicador == "Qualidade da Cobrança ou Recarga"'), x='nota', y='tema', color='tema', orientation='h',
+               labels=dict(ind_tema="Tema", nota="Nota"),
+               color_discrete_sequence=["#fff666"],
+               height=300,  # altura
+               title="Qualidade da Cobrança ou Recarga",
+               template="plotly_dark",  text_auto='.3s'
+               )
+qual1.update_layout(showlegend=False)
+qual1.update_yaxes(showticklabels=True, showgrid=False, title_text='')
+qual1.update_xaxes(visible=False, fixedrange=True, showgrid=False)
+
+qual2 = px.bar(df_isg_qualidade.query('indicador == "Qualidade da Informação ao Consumidor"'), x='nota', y='tema', color='tema', orientation='h',
+               labels=dict(ind_tema="Tema", nota="Nota"),
+               color_discrete_sequence=["#fff666"],
+               height=300,  # altura
+               title="Qualidade da Informação ao Consumidor",
+               template="plotly_dark",  text_auto='.3s'
+               )
+qual2.update_layout(showlegend=False)
+qual2.update_yaxes(showticklabels=True, showgrid=False, title_text='')
+qual2.update_xaxes(visible=False, fixedrange=True, showgrid=False)
+
+qual3 = px.bar(df_isg_qualidade.query('indicador == "Qualidade do Atendimento Digital"'), x='nota', y='tema', color='tema', orientation='h',
+               labels=dict(ind_tema="Tema", nota="Nota"),
+               color_discrete_sequence=["#fff666"],
+               height=350,  # altura
+               title="Qualidade do Atendimento Digital",
+               template="plotly_dark",  text_auto='.3s'
+               )
+qual3.update_layout(showlegend=False)
+qual3.update_yaxes(showticklabels=True, showgrid=False, title_text='')
+qual3.update_xaxes(visible=False, fixedrange=True, showgrid=False)
+
+qual4 = px.bar(df_isg_qualidade.query('indicador == "Qualidade do Atendimento Telefônico"'), x='nota', y='tema', color='tema', orientation='h',
+               labels=dict(ind_tema="Tema", nota="Nota"),
+               color_discrete_sequence=["#fff666"],
+               height=350,  # altura
+               title="Qualidade do Atendimento Telefônico",
+               template="plotly_dark",  text_auto='.3s'
+               )
+qual4.update_layout(showlegend=False)
+qual4.update_yaxes(showticklabels=True, showgrid=False, title_text='')
+qual4.update_xaxes(visible=False, fixedrange=True, showgrid=False)
+
+qual5 = px.bar(df_isg_qualidade.query('indicador == "Qualidade do Funcionamento"'), x='nota', y='tema', color='tema', orientation='h',
+               labels=dict(ind_tema="Tema", nota="Nota"),
+               color_discrete_sequence=["#fff666"],
+               height=500,  # altura
+               title="Qualidade do Funcionamento",
+               template="plotly_dark",  text_auto='.3s'
+               )
+qual5.update_layout(showlegend=False)
+qual5.update_yaxes(showticklabels=True, showgrid=False, title_text='')
+qual5.update_xaxes(visible=False, fixedrange=True, showgrid=False)
 #######################
 # Dashboard Main Panel
 
@@ -154,6 +299,39 @@ with st.expander("Notas Indicadores", expanded=True):
 
 st.markdown("# Histórico Indicadores, 2021-2023")
 
-
 with st.expander("Histórico", expanded=True):
     st.plotly_chart(hist, use_container_width=True)
+
+st.markdown("# Índice de Satisfação Geral por Prestadora e por Estado, 2023")
+
+with st.expander("Prestadora", expanded=True):
+    st.plotly_chart(oper, use_container_width=True)
+
+with st.expander("Estado", expanded=False):
+    st.plotly_chart(estado, use_container_width=True)
+
+st.markdown(
+    "# Notas das Perguntas que compõe o Índice de Satisfação Geral e Qualidade, 2023")
+
+with st.expander("Satisfação Geral", expanded=True):
+    st.plotly_chart(isg, use_container_width=True)
+
+with st.expander("Qualidade :arrow_down:", expanded=False):
+
+    col = st.columns((2.6, 2.9), gap='medium')
+
+    with col[0]:
+        st.plotly_chart(qual1, use_container_width=True)
+
+    with col[1]:
+        st.plotly_chart(qual2, use_container_width=True)
+
+    with col[0]:
+        st.plotly_chart(qual3, use_container_width=True)
+
+    with col[1]:
+        st.plotly_chart(qual4, use_container_width=True)
+
+    col2 = st.columns((5.6, 0.6), gap='medium')
+    with col2[0]:
+        st.plotly_chart(qual5, use_container_width=True)
